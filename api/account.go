4316594451a188a2f589e634cd9c2f9f6cc982bb
delete-cloud <1526706882@qq.com>
2024-12-10 15:36:09 +0800

    feat: implement user tables and create user api(include test)

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/delete-cloud/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -25,8 +26,16 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		Balance:  0,
 		Currency: req.Currency,
 	}
+
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
